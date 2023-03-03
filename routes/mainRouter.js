8df4381b990460f59b9bc263cc76569bed07baf5
mwchben <benjamwa05@gmail.com>
2023-03-03 13:40:53 +0300

    ommited reg route

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,12 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mainController = require('../controllers/mainController')
-const registrationController = require('../controllers/registrationController')
 
 router.get('/', mainController.get)
-router.get('/register', registrationController.get)
-router.post('/register', registrationController.post)
-
-
 
 module.exports = router  
